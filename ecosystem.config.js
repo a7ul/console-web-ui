cc5bce8dae6d2ebaf9b84f342339994a1a4b9136
Atul R <atulanand94@gmail.com>
2018-07-15 02:42:27 +0530

    added eco system file

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [{
+    name: 'CWUIDEMO',
+    exec_mode: 'cluster',
+    instances: 'max',
+    script: 'index.js',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }],
+
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
